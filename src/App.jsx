e7ab82e8ae6d13546638bed8e4f7b1034281bda6
Abhishek-Pundir-001 <abhishekpundirnnt@gmail.com>
2025-02-24 22:35:35 +0530

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,22 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import Cart from './Pages/Cart/Cart';
 import Placeholder from './Pages/Placeorder/Placeorder';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
-    <div className='w-[80%] app'>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/cart' element={<Cart />}/>
-        <Route path='/order' element={<Placeholder />}/>
-      </Routes>
-    </div>
+    <>
+      <div className='w-[80%] app'>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/order' element={<Placeholder />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
+
 
   )
 }
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,40 @@
+import { assets } from "../../../assets/frontend_assets/assets"
+
+function Footer() {
+    return (
+        <div className="footer-container flex flex-col items-center gap-5 text-[#d9d9d9] bg-[#323232] px-[8vw] py-5 pt-20">
+            <div className="footer-sub-container flex justify-between">
+                <div className="footer-left flex flex-col gap-5 items-start basis-[45%]">
+                    <img className="w-40" src={assets.logo} />
+                    <p className="font-serif">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam soluta, voluptatibus quod reprehenderit nisi illum consequuntur. Inventore eveniet amet aspernatur!</p>
+                    <div className="flex gap-2.5">
+                        <img className="h-10" src={assets.facebook_icon} />
+                        <img className="h-10"  src={assets.twitter_icon} />
+                        <img className="h-10"  src={assets.linkedin_icon} />
+                    </div>
+                </div>
+                <div className="footer-center flex flex-col gap-3 items-start">
+                    <h2 className="font-bold text-xl font-serif">COMPANY</h2>
+                    <ul className="flex flex-col gap-2">
+                        <li className="hover:text-orange-400 cursor-pointer font-serif">Home</li>
+                        <li className="hover:text-orange-400 cursor-pointer font-serif">About us</li>
+                        <li className="hover:text-orange-400 cursor-pointer font-serif">Delivery</li>
+                        <li className="hover:text-orange-400 cursor-pointer font-serif">Privacy policy</li>
+                    </ul>
+                </div>
+                <div className="footer-right flex flex-col gap-3 items-start">
+                    <h2 className="font-bold text-xl font-serif">GET IN TOUCH</h2>
+                    <ul className="flex flex-col gap-2">
+                        <li className="hover:text-orange-400 cursor-pointer font-serif">+1-212-456-7898</li>
+                        <li className="hover:text-orange-400 cursor-pointer font-serif">conttact@tomato.com</li>
+                    </ul>
+                </div>
+
+            </div>
+            <hr />
+            <p className="footer-copyright font-serif">Copyright 2024 &copy; Tomato.com ~ All rights are reserved</p>
+        </div>
+    )
+}
+
+export default Footer
